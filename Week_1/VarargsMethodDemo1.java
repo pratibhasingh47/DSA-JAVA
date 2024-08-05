f73fb2b525d343a3089961cb45bd5d75b36150e7
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-08-05 22:21:31 +0530

    java-dsa

--- Week_1/VarargsMethodDemo1.java
+++ Week_1/VarargsMethodDemo1.java
@@ -3,7 +3,7 @@ package Week_1;
 class VarargsMethodDemo1 {
     static void varargsMethod1(int v[]) {
         System.out.print("Number of args: " + v.length + " Elements: ");
-        for(int x : v) {
+        for (int x : v) {
             System.out.print(x + " ");
         }
         System.out.println();
@@ -13,8 +13,8 @@ class VarargsMethodDemo1 {
         // Following arrays are created for test...
         int x[] = { 1, 3, 5, 7 };
         int y[] = { 2, 4 };
-        int z[] = { };
-        
+        int z[] = {};
+
         varargsMethod1(x); // Passed 4 values to the method
         varargsMethod1(y); // Passed 2 values to the method
         varargsMethod1(z); // Passed no argument to the method
