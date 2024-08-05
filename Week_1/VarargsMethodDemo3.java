bd3a3fa155dc9f84428de0a83f898458bebbd1bf
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-08-05 22:23:01 +0530

    java

--- Week_1/VarargsMethodDemo3.java
+++ Week_1/VarargsMethodDemo3.java
@@ -1,5 +1,15 @@
 package Week_1;
 
-public class VarargsMethodDemo3 {
-    
+class VarargsMethodDemo3 {
+    public static void varargsMethod3(Object... obj) {
+        for (Object o : obj) {
+            System.out.print(" " + o);
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        varargsMethod3(1, "String", 2.3, true); // Four arguments
+        varargsMethod3(); // No arguments 
+        varargsMethod3(15, 25, 35, 45, 55); // Five arguments
+    }
 }
