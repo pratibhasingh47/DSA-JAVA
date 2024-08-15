851cf616ba6d7418262c33c413839fe20dc2e025
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-08-15 13:10:11 +0530

    java

---
+++ Week_3/RangeCopyArraysDemo.java
@@ -0,0 +1,20 @@
+package Week_3;
+
+import java.util.Arrays;
+
+public class RangeCopyArraysDemo {
+    public static void main(String[] args) {
+        int[] intArr = {10, 20, 15, 22, 35}; // Get an array of int
+
+        // To print the elements in one line
+        System.out.println("Integer Array: " + Arrays.toString(intArr));
+
+        // To copy a range of the array into a new array
+        System.out.println("\nNew Array by copyOfRange:\n");
+
+        // Copy elements from index 1 to 2 (3 is exclusive)
+        int[] rangeArr = Arrays.copyOfRange(intArr, 1, 3);
+
+        System.out.println("Integer Array: " + Arrays.toString(rangeArr));
+    }
+}
