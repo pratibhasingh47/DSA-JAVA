9e02b438a12c49c463b3c22ffcea134d2e805a56
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-08-15 13:05:51 +0530

    java

---
+++ Week_3/ArraysToStreamDemo.java
@@ -0,0 +1,19 @@
+package Week_3;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class ArraysToStreamDemo {
+    public static void main(String[] args) {
+        // Get the Array
+        int[] intArr = {10, 20, 15, 22, 35};
+
+        // To get the Stream from the array
+        IntStream intStream = Arrays.stream(intArr);
+
+        // Convert the stream to a String representation for display
+        String streamAsString = intStream.mapToObj(String::valueOf).reduce((a, b) -> a + ", " + b).orElse("");
+
+        System.out.println("Integer Array as Stream: [" + streamAsString + "]");
+    }
+}
