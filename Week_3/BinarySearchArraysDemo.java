66a403f74d92a610f19bdc2ddba11c27f163344a
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-08-16 20:45:30 +0530

    java

---
+++ Week_3/BinarySearchArraysDemo.java
@@ -0,0 +1,18 @@
+package Week_3;
+
+import java.util.Arrays;
+
+public class BinarySearchArraysDemo {
+    public static void main(String[] args) {
+        // Get the Array
+        int[] intArr = {10, 20, 15, 22, 35};
+
+        // Sort the array before performing a binary search
+        Arrays.sort(intArr);
+
+        int intKey = 22;
+
+        // Perform binary search and print the result
+        System.out.println(intKey + " found at index = " + Arrays.binarySearch(intArr, intKey));
+    }
+}
