bc67474c137331465b8bba433ae60fdd79667312
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-08-17 16:52:41 +0530

    java

---
+++ Week_3/VectorSearchDemo2.java
@@ -0,0 +1,19 @@
+package Week_3;
+
+import java.util.*;
+
+class VectorSearchDemo2 {
+    public static void main(String[] arg) {
+        // create default vector of capacity 10
+        Vector<Object> v = new Vector<>();
+        v.add(1);
+        v.add(2);
+        v.add("Oracle");
+        v.add(2);
+        v.add("Java");
+        v.add(4);
+        
+        // Checking last occurrence of 2
+        System.out.println("Last occurrence of 2 is: " + v.lastIndexOf(2));
+    }
+}
