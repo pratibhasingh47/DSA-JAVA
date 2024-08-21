9bb5bf3369c2bf7344a845e2c8a44c4c150a489d
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-08-21 22:15:52 +0530

    java

--- Week_4/JLinkedList.java
+++ Week_4/JLinkedList.java
@@ -1,5 +1,31 @@
 package Week_4;
 
-public class JLinkedList {
-    
+public class JLinkedList<T> {
+    private Node head; // Head of the list
+
+    // Inner class Node
+    private class Node {
+        T data;
+        Node next;
+
+        // Default constructor
+        Node() {
+            data = null;
+            next = null;
+        }
+
+        // Constructor with data
+        Node(T d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    // Constructor for JLinkedList
+    public JLinkedList() {
+        // Initialize head with a dummy node
+        head = new Node();
+    }
+
+    // Method to maintain the collection to be defined …
 }
