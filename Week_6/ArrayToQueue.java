484638fe87a926a3052dbda803f245882f0370cc
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-09-04 22:06:26 +0530

    java

---
+++ Week_6/ArrayToQueue.java
@@ -0,0 +1,28 @@
+package Week_6;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class ArrayToQueue {
+    public static void main(String[] args) {
+        // Create an array of String objects
+        String[] city = {"CCU", "DEL", "BLR", "MUM", "GAU"};
+
+        // Declare a queue
+        Queue<String> queue = new PriorityQueue<>();
+
+        // Copying the array to the Queue
+        Collections.addAll(queue, city);
+
+        // Printing the array
+        System.out.println("Array:");
+        for (String c : city) {
+            System.out.print(c + " ");
+        }
+        System.out.println();
+
+        // Printing the queue
+        System.out.println("Queue: " + queue);
+    }
+}
