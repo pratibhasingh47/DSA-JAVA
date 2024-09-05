19ff759e8ffc267f29cfe1e915e3abbc779cf5f1
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-09-05 21:18:58 +0530

    java

---
+++ Week_6/QueueToArray.java
@@ -0,0 +1,27 @@
+package Week_6;
+
+import java.util.*;
+
+public class QueueToArray {
+    public static void main(String[] args) {
+        // Creating a queue
+        Queue<String> queue = new LinkedList<>();
+
+        // Adding elements to the queue
+        queue.add("Jio");
+        queue.add("123");
+        queue.add("John");
+        queue.add("Jaya");
+        queue.add("Jim");
+        queue.add("!@#$%");
+
+        // Copying the queue to an array of String objects
+        String[] friends = queue.toArray(new String[0]);
+
+        // Printing the array
+        System.out.println("Array: " + Arrays.toString(friends));
+        
+        // Printing the queue
+        System.out.println("Queue: " + queue);
+    }
+}
