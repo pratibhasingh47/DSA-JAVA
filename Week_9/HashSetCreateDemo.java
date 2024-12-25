b699b5a0481b51fcd43fa8057952dae23987c867
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-12-25 21:24:37 +0530

    dsa_java

---
+++ Week_9/HashSetCreateDemo.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class HashSetCreateDemo {
+    public static void main(String[] args) {
+        // Create a hash set.
+        Set<String> hs = new HashSet<>();
+
+        // Add elements to the hash set.
+        hs.add("India");
+        hs.add("Japan");
+        hs.add("Australia");
+        hs.add("Bangladesh");
+        hs.add("Pakistan");
+        hs.add("Nepal");
+
+        // Print the hash set.
+        System.out.println(hs);
+    }
+}
