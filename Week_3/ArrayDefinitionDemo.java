f6baea4bd4da685b657668f29cd166b0a09e8383
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-12-28 18:50:09 +0530

    dsa_java

--- Week_3/ArrayDefinitionDemo.java
+++ Week_3/ArrayDefinitionDemo.java
@@ -9,16 +9,16 @@ public class ArrayDefinitionDemo {
 
         boolean[] isPrime = new boolean[1000];
 
-        int[] fib = {0, 1, 1, 2, 3, 5, 8, 13};
+        int[] fib = { 0, 1, 1, 2, 3, 5, 8, 13 };
 
         short[][][] b = new short[4][10][5];
 
-        double[][] a = {{1.1, 2.2}, {3.3, 4.4}, null, {5.5, 6.6}, null};
+        double[][] a = { { 1.1, 2.2 }, { 3.3, 4.4 }, null, { 5.5, 6.6 }, null };
 
         a[4] = new double[66];
         a[4][65] = 3.14;
 
         @SuppressWarnings("unused")
-        Object[] objects = {x, args, isPrime, fib, b, a};
+        Object[] objects = { x, args, isPrime, fib, b, a };
     }
 }
