9f9db5be468fca4824f4eeb6b692a4a7d0c969d5
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-12-29 23:27:49 +0530

    dsa_java

--- Week_3/ArraysToStringDemo.java
+++ Week_3/ArraysToStringDemo.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class ArraysToStringDemo {
     public static void main(String[] args) {
         // Get the Array
-        int[] intArr = {10, 20, 15, 22, 35};
+        int[] intArr = { 10, 20, 15, 22, 35 };
 
         // To print the elements in one line
         System.out.println("Integer Array: " + Arrays.toString(intArr));
