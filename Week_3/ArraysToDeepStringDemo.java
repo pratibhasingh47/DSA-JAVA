87bfb0520f35ea5c30eb28394a9bc6911886b0b8
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-12-29 23:27:23 +0530

    dsa_java

--- Week_3/ArraysToDeepStringDemo.java
+++ Week_3/ArraysToDeepStringDemo.java
@@ -3,9 +3,9 @@ package Week_3;
 import java.util.Arrays;
 
 public class ArraysToDeepStringDemo {
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
         // Get the Array
-        int[][] intArr = { {10, 20, 15, 22, 35} };
+        int[][] intArr = { { 10, 20, 15, 22, 35 } };
 
         // To get the deep String representation of the arrays
         System.out.println("Integer Array: " + Arrays.deepToString(intArr));
