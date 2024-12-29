880374c959b2e4b1ca1f7d806b5a7d44fcf62345
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-12-29 23:27:08 +0530

    dsa_java

--- Week_3/ArraysOfHashcodeDemo.java
+++ Week_3/ArraysOfHashcodeDemo.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class ArraysOfHashcodeDemo {
     public static void main(String[] args) {
         // Get the Array
-        int[][] intArr = {{10, 20, 15, 22, 35}};
+        int[][] intArr = { { 10, 20, 15, 22, 35 } };
 
         // To get the deep hashCode of the arrays
         System.out.println("Integer Array deep hashCode: " + Arrays.deepHashCode(intArr));
