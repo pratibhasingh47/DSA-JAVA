0d83c4e19f9978d888996767c2b5e38bfc83b5ff
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-12-30 21:16:30 +0530

    dsa_java

--- Week_3/CopyOfArraysDemo.java
+++ Week_3/CopyOfArraysDemo.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class CopyOfArraysDemo {
     public static void main(String[] args) {
-        int[] intArr = {10, 20, 15, 22, 35}; // An input array
+        int[] intArr = { 10, 20, 15, 22, 35 }; // An input array
 
         // To print the elements in one line
         System.out.println("Integer Array: " + Arrays.toString(intArr));
