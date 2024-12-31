50f5aaaf4093f5d489028dd84e901dd805cbde9a
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-12-31 23:44:51 +0530

    dsa_java

--- Week_3/DeletionArrayListDemo.java
+++ Week_3/DeletionArrayListDemo.java
@@ -6,7 +6,7 @@ public class DeletionArrayListDemo {
     public static void main(String[] args) {
         // Create a collection. Initially empty
         ArrayList<String> langs = new ArrayList<String>();
-        
+
         // Add elements into the collection
         langs.add("C");
         langs.add("C++");
@@ -16,15 +16,15 @@ public class DeletionArrayListDemo {
         langs.add("Spark");
 
         System.out.println("Initial List: " + langs);
-        
+
         // Remove the element at index `5`
-        langs.remove(5); 
+        langs.remove(5);
         System.out.println("After remove(5): " + langs);
-        
+
         // Remove the first occurrence of the given element from the ArrayList
         boolean status = langs.remove("Smalltalk");
         System.out.println("Smalltalk is removed: " + status);
-        
+
         // Remove all the elements that exist in a given collection
         ArrayList<String> script = new ArrayList<String>();
         script.add("SQL");
@@ -32,11 +32,11 @@ public class DeletionArrayListDemo {
         script.add("Javascript");
         langs.removeAll(script); // Remove intersection of langs and script
         System.out.println("After script removal: " + langs);
-        
+
         // Remove all elements that start with "C"
         langs.removeIf(s -> s.startsWith("C"));
         System.out.println("After Removing all elements that start with \"C\": " + langs);
-        
+
         // Remove all elements from the ArrayList
         langs.clear();
         System.out.println("List is empty? " + langs.isEmpty());
