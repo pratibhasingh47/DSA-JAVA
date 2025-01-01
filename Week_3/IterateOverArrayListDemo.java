930693f657a6505ffa2700ea1c6c005d8e3694aa
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2025-01-01 22:34:16 +0530

    dsa_java

--- Week_3/IterateOverArrayListDemo.java
+++ Week_3/IterateOverArrayListDemo.java
@@ -7,17 +7,17 @@ import java.util.ListIterator;
 public class IterateOverArrayListDemo {
     public static void main(String[] args) {
         ArrayList<String> tvShows = new ArrayList<>();
-        
+
         // Adding elements to the ArrayList
         tvShows.add("Nimki Mukhiya");
         tvShows.add("Game of Thrones");
         tvShows.add("Mahabharat");
         tvShows.add("Balika Badhu");
-        
+
         // Traversing using forEach() and lambda
         System.out.println("Traversing using forEach() and lambda:");
         tvShows.forEach(tvShow -> System.out.println(tvShow));
-        
+
         // Iterate using an iterator
         System.out.println("\n=== Iterate using an iterator() ===");
         Iterator<String> tvShowIterator = tvShows.iterator();
@@ -25,12 +25,12 @@ public class IterateOverArrayListDemo {
             String tvShow = tvShowIterator.next();
             System.out.println(tvShow);
         }
-        
+
         // Traversing using iterator() and forEachRemaining()
         System.out.println("\nTraversing using iterator() and forEachRemaining():");
         tvShowIterator = tvShows.iterator();
         tvShowIterator.forEachRemaining(tvShow -> System.out.println(tvShow));
-        
+
         // Traversing using a listIterator()
         System.out.println("\nTraversing using a listIterator() backwards:");
         ListIterator<String> tvShowListIterator = tvShows.listIterator(tvShows.size());
@@ -38,13 +38,13 @@ public class IterateOverArrayListDemo {
             String tvShow = tvShowListIterator.previous();
             System.out.println(tvShow);
         }
-        
+
         // Iterate using simple for-each loop
         System.out.println("\n=== Iterate using simple for-each loop ===");
         for (String tvShow : tvShows) {
             System.out.println(tvShow);
         }
-        
+
         // Iterate using for loop with index
         System.out.println("\n=== Iterate using for loop with index ===");
         for (int i = 0; i < tvShows.size(); i++) {
