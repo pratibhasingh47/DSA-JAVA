9f9386f88ef42373df2076805e71fd5a6222ae33
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2025-01-01 22:34:00 +0530

    dsa_java

--- Week_3/InsertiontoArraysDemo.java
+++ Week_3/InsertiontoArraysDemo.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class InsertiontoArraysDemo {
     public static void main(String[] args) {
         // Get the Array
-        int[] intArr = {10, 20, 15, 22, 35};
+        int[] intArr = { 10, 20, 15, 22, 35 };
         int intKey = 22;
 
         // Fill the array with the specified value
