463cb38fc21a6c31562fd38c049554d4d3843416
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2025-01-02 22:05:41 +0530

    dsa_java

--- Week_3/TraverseSpliterator.java
+++ Week_3/TraverseSpliterator.java
@@ -6,7 +6,7 @@ import java.util.Spliterator;
 public class TraverseSpliterator {
     public static void main(String[] args) {
         // Get the Array
-        int[] intArr = {10, 20, 15, 22, 35};
+        int[] intArr = { 10, 20, 15, 22, 35 };
 
         // Get a Spliterator from the array using IntStream
         Spliterator.OfInt spliterator = Arrays.spliterator(intArr);
