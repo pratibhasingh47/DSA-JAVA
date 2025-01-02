189f0ef01f6f0b4768015b68ca71235dd95dc094
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2025-01-02 22:05:11 +0530

    dsa_java

--- Week_3/SortingArraysDemo.java
+++ Week_3/SortingArraysDemo.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class SortingArraysDemo {
     public static void main(String[] args) {
         // Get the Array
-        int[] intArr = {10, 20, 15, 22, 35};
+        int[] intArr = { 10, 20, 15, 22, 35 };
 
         // To sort the array using normal sort
         Arrays.sort(intArr);
