125bd6763cd6cfe31a0a24b586caa3c8acffadab
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2025-01-27 21:08:37 +0530

    dsa_java

--- Week_3/VectorInsertionDemo2.java
+++ Week_3/VectorInsertionDemo2.java
@@ -10,7 +10,7 @@ public class VectorInsertionDemo2 {
         // Add elements at specific indices
         v.add(1); // Add 1 at index 0 (corrected from the original code)
         v.add(2); // Add 2 at index 1
-        v.add(0, "Debasis"); // Add "Debasis" at index 0, shifting existing elements
+        v.add(0, "Debais"); // Add "Debasis" at index 0, shifting existing elements
         v.add(3, "Samanta"); // Add "Samanta" at index 3
         v.add(4, 3); // Add 3 at index 4
         v.add(5, 6.9); // Add 6.9 at index 5 (Vector grows automatically)
